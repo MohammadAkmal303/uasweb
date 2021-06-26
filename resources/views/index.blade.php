548837770152c2ae238a25730444218996ad6b66
mochfarrel10 <mochfarrel008@webmail.umm.ac.id>
2021-06-27 00:31:27 +0700

    CSS & JS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
     <title>Dya Catering</title>
   </head>
   <body>
@@ -72,7 +72,7 @@
         @endif
       <!-- Daftar Menu -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js"></script>
-      <script src="{{asset('js/app.js')}}"></script>
+      <script src="js/app.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
   </body>
 </html>
